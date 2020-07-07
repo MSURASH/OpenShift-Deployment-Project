e0e5d86fc7d2cb75cf68c848642e804dc2af80f7
MSURASH <sufian100100@gmail.com>
2020-07-07 18:28:48 -0400

    Commit All

---
+++ src/main/java/com/java/controller/CustomRestController.java
@@ -0,0 +1,19 @@
+package com.java.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomRestController {
+
+	@Value("${custom.val}")
+	private String val;
+	
+	@GetMapping("/get")
+	public ResponseEntity<String> print() {
+		
+		return ResponseEntity.ok().body(val);
+	}
+}
